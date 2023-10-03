903686ffa9e41c2ec7c91e01979bbcaf140b5b4f
Nurzhas Nurbayev <montishok@gmail.com>
2023-10-03 17:36:59 +0600

    getting to know Classes

--- public/app.js
+++ public/app.js
@@ -1,4 +1,23 @@
 "use strict";
+// Classes
+class Invoice {
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} money for ${this.details}`;
+    }
+}
+const invoice1 = new Invoice("Nurzhas", "Lunch", 2000);
+const invoice2 = new Invoice("Magzhan", "Tennis", 3000);
+let invoices = [];
+invoices.push(invoice1);
+invoices.push(invoice2);
+invoices.forEach(invoice => {
+    console.log(invoice.format());
+});
 const form = document.querySelector('.new-item-form');
 const type = document.querySelector('#type');
 const toFrom = document.querySelector('#tofrom');
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,27 @@
+// Classes
+class Invoice {
+    constructor(
+        readonly client: string,
+        readonly details: string,
+        readonly amount: number,
+    ) {}
+
+    format(): string {
+        return `${this.client} owes $${this.amount} money for ${this.details}`;
+    }
+}
+
+const invoice1 = new Invoice("Nurzhas", "Lunch", 2000);
+const invoice2 = new Invoice("Magzhan", "Tennis", 3000);
+
+let invoices: Invoice[] = [];
+invoices.push(invoice1);
+invoices.push(invoice2);
+
+invoices.forEach(invoice => {
+    console.log(invoice.format());
+});
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
 const type = document.querySelector('#type') as HTMLSelectElement;
@@ -8,7 +32,6 @@ const button = document.querySelector('#add') as HTMLButtonElement;
 
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
-
     console.log(
         type.value,
         toFrom.value,
